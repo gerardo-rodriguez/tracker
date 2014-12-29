b39742887dabbcbf05871314b240723e7d9430d2
Gerardo Rodriguez <ger.rod34@gmail.com>
2014-12-28 23:38:25 -0800

    Add ethnicity model. Migrate db to add grade and ethnicity.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229061927) do
+ActiveRecord::Schema.define(version: 20141229073712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ethnicities", force: true do |t|
+    t.string   "group"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "grades", force: true do |t|
+    t.string   "level"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
