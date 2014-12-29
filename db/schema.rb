1e20c0c5fcf3594173cd0fb57de70b84b7c95390
Gerardo Rodriguez <ger.rod34@gmail.com>
2014-12-28 23:49:31 -0800

    Generate gender model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229073712) do
+ActiveRecord::Schema.define(version: 20141229074913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ethnicities", force: true do |t|
     t.string   "group"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genders", force: true do |t|
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
