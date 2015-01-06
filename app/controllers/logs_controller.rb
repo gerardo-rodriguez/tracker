822e765cc200c0dc31f4833430419ca7eafe9460
Gerardo Rodriguez <ger.rod34@gmail.com>
2015-01-06 08:17:39 -0800

    Add student SSID to form.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,10 +7,21 @@ class LogsController < ApplicationController
 
 	def new
 		@log = Log.new
+		@student = Student.new
 	end
 
 	def create
 		@log = Log.new(log_params)
+		
+		@student = Student.where(:ssid => params[:student][:ssid])
+		if @student.exists?
+			# Add to the join table with current student id
+			@log.students << @student
+		else
+			# Add to the student table a new student
+			# Add to the join table with new student id
+			@log.students.build(student_params)
+		end
 		@log.save
 
 		redirect_to @log
@@ -23,5 +34,9 @@ class LogsController < ApplicationController
 	private
 		def log_params
 			params.require(:log).permit(:description)
+		end
+
+		def student_params
+			params.require(:student).permit(:ssid)
 		end
 end
